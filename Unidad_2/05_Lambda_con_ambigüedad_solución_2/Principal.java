f2b18d00df53600847c1f1f0db39183634063e83
Alx-Ang <segundo356alx@gmail.com>
2022-04-22 20:26:28 -0500

    Ejemplo lambda con ambigüedad solución 2 casting de método

---
+++ Unidad_2/05_Lambda_con_ambigüedad_solución_2/Principal.java
@@ -0,0 +1,24 @@
+public class Principal{
+    public static void main(String[] args) {
+        Principal.engine((CalculadoraInt)(x, y) -> x + y);
+        Principal.engine((CalculadoraInt)(x, y) -> x * y);
+        Principal.engine((CalculadoraInt)(x, y) -> x / y);
+        Principal.engine((CalculadoraLong)(x, y) -> x - y);
+        Principal.engine((CalculadoraLong)(x, y) -> x % y);
+
+    }
+
+    // Sobrecarga de métodos
+    private static void engine(CalculadoraInt cal){
+        int x = 2, y = 4;
+        int resultado = cal.calcular(x, y);
+        System.out.println("Resultado = " + resultado);
+    }
+    
+    private static void engine(CalculadoraLong cal){
+        long x = 2, y = 4;
+        long resultado = cal.calcular(x, y);
+        System.out.println("Resultado = " + resultado);
+    }
+    
+}
