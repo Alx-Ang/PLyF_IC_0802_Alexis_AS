4ec751f068ef7b41be04fb7de116d9f9a692e9bf
Alx-Ang <segundo356alx@gmail.com>
2022-04-26 16:41:15 -0500

    Ejercicios lambda

---
+++ Unidad_2/05_Lambda_Ejercicios/01_Ejercicio_Lambda_c1/CalculadoraLong.java
@@ -0,0 +1,5 @@
+/* Interfaz funcional */
+
+public interface CalculadoraLong{
+    public long calcular(long x1, long y2);
+}
---
+++ Unidad_2/05_Lambda_Ejercicios/02_Ejercicio_Lambda_c2/CalculadoraInt.java
@@ -0,0 +1,5 @@
+/* Interfaz funcional */
+
+public interface CalculadoraInt{
+    public int calculate(int x, int y);
+}
