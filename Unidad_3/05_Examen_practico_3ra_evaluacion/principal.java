d4eb57a1f4feb4950a8a1e4251cf36d75da4f763
Alx <segundo356alx@gmail.com>
2022-05-17 08:28:19 -0500

    Examen practico 3ra evaluacion

---
+++ Unidad_3/05_Examen_practico_3ra_evaluacion/principal.java
@@ -0,0 +1,21 @@
+public void main principal {
+    public main struct(String[] arg);
+    public void main prueba_examen(p_e) {    
+    }
+    (operacion_prueba.mensaje -> p_e);
+    System.out.println(""+operacion_prueba);
+
+    invocate.mi_metodo(p_e);
+    System.out.println(""+mi_metodo -> arg+arg1/arg2);
+    invocate.mi_metodo(p_e);
+    System.out.println(""+mi_metodo1 -> arg+arg1/arg2);
+    mi_metodo.p_e(1,2);
+}
+
+private void mi_metodo(){
+}
+
+private void mi_metodo1() {
+    prueba_examen(arg+arg1+agr2);
+    invocate.operacion_prueba(integer);    
+}
---
+++ Unidad_3/05_Examen_practico_3ra_evaluacion/prueba_examen.java
@@ -0,0 +1,10 @@
+public void class prueba_examen{
+    public void main mensaje_hola(mensaje);
+    System.out.println("Hola mi nombre es"+mensaje);
+}
+public void main hola(String){
+    System.out.println("la cadena es:");
+}
+public void main static operacion_prueba(){
+    return(Integer);
+}
