c3da4bf5499baab12ae362f259bd021faef67e32
Alx <segundo356alx@gmail.com>
2022-06-07 09:14:41 -0500

    Ejecicio prolog .pl

---
+++ Unidad_4/01_Prolog/amigos.pl
@@ -0,0 +1,32 @@
+%% Afirmación de predicados amigos de pedro
+amigos(pedro,antonio).
+amigos(pedro,floras).
+amigos(pedro,juan).                     
+amigos(pedro,vicente).
+
+%% Afirmación de predicados amigos de luis
+amigos(luis,felipe).
+amigos(luis,maria).
+amigos(luis,vicente).
+
+%% Afirmación de predicados amigos de carlos
+amigos(carlos,paloma).
+amigos(carlos,lucia).
+amigos(carlos,juan).
+amigos(carlos,vicente).
+
+%% Afirmación de predicados amigos de fernando
+amigos(fernando,eva).
+amigos(fernando,pedro).
+
+%% Afirmación de predicados amigos millonarios
+millonario(pedro).
+millonario(antonio).
+millonario(flora).
+
+%%Afirmación de predicados amigos solteros
+soltero(pedro).
+soltero(flora).
+soltero(eva).
+soltero(luis).
+soltero(juan).
